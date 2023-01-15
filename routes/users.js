3c2fd596983387fba38d72b1c6af4f2d1c85ce9e
Karthik Raja <karthikrajaamd@gmail.com>
2023-01-15 13:12:03 +0530

    email

--- routes/users.js
+++ routes/users.js
@@ -98,7 +98,7 @@ router.post("/sendpasswordlink", async (req, res) => {
       // );
       const sendLink = await nodemailerService(
         userfind.email,
-        `http://localhost:3001/users/forgotpassword/${userfind.id}/${setusertoken.verifytoken}`
+        `http://https://raja-ordermanagement.netlify.app/users/forgotpassword/${userfind.id}/${setusertoken.verifytoken}`
       );
       res.send({
         statusCode: 200,
