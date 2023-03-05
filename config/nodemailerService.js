fd5fa36669fadacf2794d31e1dc7ba0afb1caf45
Karthik Raja <karthikrajaamd@gmail.com>
2023-03-05 22:31:27 +0530

    added payment

--- config/nodemailerService.js
+++ config/nodemailerService.js
@@ -1,39 +1,33 @@
 // "use strict";
 const nodemailer = require("nodemailer");
-
+const dotenv = require("dotenv").config();
 // async..await is not allowed in global scope, must use a wrapper
 const nodemailerService = async (toAddress, myLink) => {
   try {
-    // Generate test SMTP service account from ethereal.email
-    // Only needed if you don't have a real mail account for testing
-    // let testAccount = await nodemailer.createTestAccount();
-
-    // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
       service: "gmail",
+      host: "smtp.gmail.com",
+      secure: "true",
       auth: {
-        user: "karthikraja.a.ece@gmail.com", // generated ethereal user
-        pass: "jpxdjdhpfuizgnld", // generated ethereal password
+        user: process.env.MAIL_USERNAME, // generated ethereal user
+        pass: process.env.MAIL_PASS, // generated ethereal password
       },
     });
 
     // send mail with defined transport object
     let info = await transporter.sendMail({
-      from: "karthikraja.a.ece@gmail.com", // sender address
+      from: process.env.MAIL_USERNAME, // sender address
       to: `${toAddress}`, // list of receivers
       subject: "Hello ✔", // Subject line
-      text: "Hello world?", // plain text body
+      text: "Reset Link?", // plain text body
       html: `<b>${myLink}</b>`, // html body
     });
 
     console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-    // Preview only available when sending through an Ethereal account
     console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
   } catch (error) {
-    console.error("error da", error);
+    console.error("error", error);
   }
 };
 
